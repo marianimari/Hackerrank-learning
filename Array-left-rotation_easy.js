d26012b001d18d64d2f31d1c622ed4992e879ce1
mariani <m.mariani@global-coding.com>
2022-01-25 15:55:25 +0800

    solve hackerrank challenge on Array: left over, easy level

---
+++ Array-left-rotation_easy.js
@@ -0,0 +1,18 @@
+function rotLeft(a, d) {
+    // Write your code here
+    let a2 = [];
+    for (let i = 0; i < d; i++) {
+        for (let j = 0; j < a.length; j++) {
+           if(j===0){
+               a2[a.length-1]=a[j];
+           } 
+           else{
+               a2[j-1]=a[j];
+           }
+        }
+        a=a2;
+        a2=[];
+    }
+    
+    return a;
+}
